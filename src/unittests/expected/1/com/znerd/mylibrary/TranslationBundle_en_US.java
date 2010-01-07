0768e22734760151b11e829bc74be90e5142c2ba
Ernst de Haan <ernst@pensioenpage.com>
2010-01-07 12:45:53 +0100

    Updated expected TranslationBundle_en_US source file for unit test 1.

--- src/unittests/expected/1/com/znerd/mylibrary/TranslationBundle_en_US.java
+++ src/unittests/expected/1/com/znerd/mylibrary/TranslationBundle_en_US.java
@@ -1,17 +1,16 @@
-
-	package com.znerd.mylibrary;
+package com.znerd.mylibrary;
 
 /**
- * Translation bundle for the <em></em> locale.
+ * Translation bundle for the <em>en_US</em> locale.
  *
  * @see Log
  */
-public final class TranslationBundle_ extends TranslationBundle {
+public final class TranslationBundle_en_US extends TranslationBundle {
 
    /**
     * The one and only instance of this class.
     */
-   public static final TranslationBundle_ SINGLETON = new TranslationBundle_();
+   public static final TranslationBundle_en_US SINGLETON = new TranslationBundle_();
 
 
    /**
@@ -20,26 +19,56 @@ public final class TranslationBundle_ extends TranslationBundle {
     * functions should be used.
     */
    private TranslationBundle_() {
-      super("");
+      super("en_US");
+   }
+
+   public String translation_100() {
+      StringBuffer buffer = new StringBuffer(255);
+      buffer.append("Initializing transaction system.");
+      return buffer.toString();
+   }
+
+   public String translation_101() {
+      StringBuffer buffer = new StringBuffer(255);
+      buffer.append("Transaction system initialized in ");
+      if (duration == null) {
+         buffer.append("(null)");
+      } else {
+         buffer.append(duration);
+      }
+      buffer.append(" ms.");
+      return buffer.toString();
+   }
+
+   public String translation_102() {
+      StringBuffer buffer = new StringBuffer(255);
+      buffer.append("Starting transaction ");
+      if (id == null) {
+         buffer.append("(null)");
+      } else {
+         buffer.append(id);
+      }
+      buffer.append(" (description: ");
+      if (description == null) {
+         buffer.append("(null)");
+      } else {
+         buffer.append('"');
+         buffer.append(description);
+         buffer.append('"');
+      }
+      buffer.append(").");
+      return buffer.toString();
+   }
+
+   public String translation_103() {
+      StringBuffer buffer = new StringBuffer(255);
+      buffer.append("Transaction ");
+      if (id == null) {
+         buffer.append("(null)");
+      } else {
+         buffer.append(id);
+      }
+      buffer.append(" fully started.");
+      return buffer.toString();
    }
 }
-
-
-	
-		
-			Transaction system initializing.
-		
-		
-			Transaction system initialized.
-			
-		
-		
-			Transaction starting.
-			
-			
-		
-		
-			Transaction fully started.
-			
-		
-	
