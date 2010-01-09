e82779c20fc817cd6771525bee5a0ffcb90a44f6
Ernst de Haan <ernst@pensioenpage.com>
2010-01-09 12:00:49 +0100

    Added InternalLogging class.

---
+++ src/java/org/znerd/logdoc/internal/InternalLogging.java
@@ -0,0 +1,86 @@
+// See the COPYRIGHT file for copyright and license information
+package org.znerd.logdoc.internal;
+
+/**
+ * Logdoc-internal logging.
+ *
+ * @author <a href="mailto:ernst@ernstdehaan.com">Ernst de Haan</a>
+ */
+public final class InternalLogging {
+   
+   //-------------------------------------------------------------------------
+   // Class functions
+   //-------------------------------------------------------------------------
+
+   /**
+    * Logs a debug message.
+    * 
+    * @param message
+    *    the message, or <code>null</code>.
+    */
+   public static void debug(String message) {
+      // TODO
+   }
+
+   /**
+    * Logs an informational message.
+    * 
+    * @param message
+    *    the message, or <code>null</code>.
+    */
+   public static void info(String message) {
+      // TODO
+   }
+   
+   /**
+    * Logs an informational message that should typically be noticed.
+    * 
+    * @param message
+    *    the message, or <code>null</code>.
+    */
+   public static void notice(String message) {
+      // TODO
+   }
+   
+   /**
+    * Logs a warning message.
+    * 
+    * @param message
+    *    the message, or <code>null</code>.
+    */
+   public static void warn(String message) {
+      // TODO
+   }
+
+   /**
+    * Logs an error message.
+    * 
+    * @param message
+    *    the message, or <code>null</code>.
+    */
+   public static void error(String message) {
+      // TODO
+   }
+
+   /**
+    * Logs a fatal error message.
+    * 
+    * @param message
+    *    the message, or <code>null</code>.
+    */
+   public static void fatal(String message) {
+      // TODO
+   }
+
+   
+   //-------------------------------------------------------------------------
+   // Constructors
+   //-------------------------------------------------------------------------
+
+   /**
+    * Constructs a new <code>InternalLogging</code> object.
+    */
+   private InternalLogging() {
+      // empty
+   }
+}
