4b3e6fdc322669d478ab63e1ae3d552a8e3db7ea
Ernst de Haan <ernst@pensioenpage.com>
2010-01-13 22:34:38 +0100

    Allowing different InternalLogging implementations. Preparation for Ant-specific logger.

--- src/java/org/znerd/logdoc/internal/InternalLogging.java
+++ src/java/org/znerd/logdoc/internal/InternalLogging.java
@@ -8,7 +8,16 @@ import org.znerd.logdoc.LogLevel;
  *
  * @author <a href="mailto:ernst@ernstdehaan.com">Ernst de Haan</a>
  */
-public final class InternalLogging {
+public class InternalLogging {
+   
+   //-------------------------------------------------------------------------
+   // Class fields
+   //-------------------------------------------------------------------------
+
+   /**
+    * The singleton instance. Never <code>null</code>.
+    */
+   private static InternalLogging INSTANCE;
    
    //-------------------------------------------------------------------------
    // Class functions
@@ -43,18 +52,34 @@ public final class InternalLogging {
     *    the exception to log, or <code>null</code> if there is no exception
     *    to log.
     */
-   public static void log(LogLevel level, String message, Throwable exception) {
+   public static synchronized void log(LogLevel level, String message, Throwable exception) {
       if (level == null) {
          level = LogLevel.DEBUG;
       }
       if (message == null) {
          message = "";
       }
-      System.err.println(level.name() + ' ' + message);
-      if (exception != null) {
-         exception.printStackTrace();
-      }
+
+      INSTANCE.logImpl(level, message, exception);
    }
+   
+   /**
+    * Sets the <code>InternalLogging</code> instance to use.
+    * 
+    * @param logger
+    *    the instance to use, cannot be <code>null</code>.
+    *    
+    * @throws IllegalArgumentException
+    *    if <code>logger == null</code>.
+    */
+   public static synchronized void setLogger(InternalLogging logger)
+   throws IllegalArgumentException {
+      if (logger == null) {
+         throw new IllegalArgumentException("logger == null");
+      }
+      INSTANCE = logger;
+   }
+
 
    
    //-------------------------------------------------------------------------
@@ -64,7 +89,34 @@ public final class InternalLogging {
    /**
     * Constructs a new <code>InternalLogging</code> object.
     */
-   private InternalLogging() {
+   protected InternalLogging() {
       // empty
+   }
+   
+   
+   //-------------------------------------------------------------------------
+   // Methods
+   //-------------------------------------------------------------------------
+
+   /**
+    * Logs a message at a specified level, with an optional exception.
+    * 
+    * @param level
+    *    the {@link LogLevel}, <code>null</code> is equivalent to passing
+    *    {@link LogLevel#DEBUG}.
+    * 
+    * @param message
+    *    the message, or <code>null</code> if an empty message should be
+    *    logged.
+    *    
+    * @param exception
+    *    the exception to log, or <code>null</code> if there is no exception
+    *    to log.
+    */
+   public void logImpl(LogLevel level, String message, Throwable exception) {
+      System.err.println(level.name() + ' ' + message);
+      if (exception != null) {
+         exception.printStackTrace();
+      }      
    }
 }
