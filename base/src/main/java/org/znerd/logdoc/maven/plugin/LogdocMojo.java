5ffec5e54edb37c3deab5ad7f65df76839de1fe8
Ernst de Haan <ernst@ernstdehaan.com>
2010-05-22 17:06:26 +0200

    Splitting up modules. (wip)

---
+++ base/src/main/java/org/znerd/logdoc/maven/plugin/LogdocMojo.java
@@ -0,0 +1,21 @@
+// See the COPYRIGHT file for copyright and license information
+package org.znerd.logdoc.maven.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Says "Hi" to the user.
+ *
+ * @goal generateLogdocSourceFiles
+ *
+ * @author <a href="mailto:ernst@ernstdehaan.com">Ernst de Haan</a>
+ */
+public class LogdocMojo extends AbstractMojo {
+
+   @Override
+   public void execute() throws MojoExecutionException {
+      getLog().info("Hello, world.");
+   }
+}
+
