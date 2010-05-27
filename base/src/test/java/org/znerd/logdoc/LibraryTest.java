8b7a5b9f9b43f024b15295fd35bc74ed9683a836
Ernst de Haan <ernst@ernstdehaan.com>
2010-05-27 23:28:58 +0200

    Moved Library.getVersion() test back from integration-test to unit-test.

--- base/src/test/java/org/znerd/logdoc/LibraryTest.java
+++ base/src/test/java/org/znerd/logdoc/LibraryTest.java
@@ -27,6 +27,32 @@ public final class LibraryTest {
    }
 
    /**
+    * Tests that the version of the library is valid.
+    */
+   @Test
+   public void testVersion() {
+
+      // Library version not null and not empty
+      assertNotNull("Library.getVersion() == null", Library.getVersion());
+      assertTrue("Library.getVersion() has length 0.", Library.getVersion().length() > 0);
+
+      // First test the pattern
+      String pattern = "^0|[1-9][0-9]*(\\.0|[1-9][0-9]*)(-SNAPSHOT)?$";
+      assertTrue("0".matches(pattern));
+      assertTrue("0.0".matches(pattern));
+      assertTrue("1.0".matches(pattern));
+      assertTrue("11.0".matches(pattern));
+      assertTrue("11.1.2.3.4.5.6.7.8.9".matches(pattern));
+      assertTrue("3.4.5.6.79-SNAPSHOT".matches(pattern));
+      assertFalse("a0".matches(pattern));
+      assertFalse("0a".matches(pattern));
+      assertFalse("11.00.1.2".matches(pattern));
+
+      // ...then actually use the pattern to test the version string
+      assertTrue(Library.getVersion().matches("^0|[1-9][0-9]*(\\.0|[1-9][0-9]*)(-SNAPSHOT)?$"));
+   }
+
+   /**
     * Tests modification and retrieval of the <em>locale</em> setting in the 
     * <code>Library</code> class.
     */
