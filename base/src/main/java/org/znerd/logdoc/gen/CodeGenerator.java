9bf1ebac78c51291a64884009a98771fadb36b7d
Ernst de Haan <ernst@ernstdehaan.com>
2011-02-24 23:13:21 +0100

    Refactored LogDef class, created new CodeGenerator class and introduced 'gen' package.

---
+++ base/src/main/java/org/znerd/logdoc/gen/CodeGenerator.java
@@ -0,0 +1,114 @@
+// See the COPYRIGHT file for copyright and license information
+package org.znerd.logdoc.gen;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import org.xml.sax.SAXException;
+
+import org.znerd.logdoc.LogDef;
+import org.znerd.logdoc.Resolver;
+
+/**
+ * Code generator.
+ *
+ * @author <a href="mailto:ernst@ernstdehaan.com">Ernst de Haan</a>
+ */
+public final class CodeGenerator {
+
+   public CodeGenerator(Resolver resolver) {
+      _resolver = resolver;
+   }
+
+   private final Resolver _resolver;
+
+   /**
+    * Generates the Java code for the specified log definition.
+    *
+    * @param def
+    *    the {@link LogDef} object, cannot be <code>null</code>.
+    *
+    * @param target
+    *    the target to generate code for, e.g. <code>"log4j"</code> or
+    *    <code>"slf4j"</code>, cannot be <code>null</code>.
+    *
+    * @param targetDir
+    *    the target directory to create the Java source files in,
+    *    cannot be <code>null</code>, and must be an existent writable
+    *    directory.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>def == null || target == null || targetDir == null</code>.
+    *
+    * @throws IOException
+    *    if the Java code could not be generated.
+    */
+   public void generateCode(LogDef def, String target, File targetDir)
+   throws IllegalArgumentException, IOException {
+
+      // Check preconditions
+      if (target == null) {
+         throw new IllegalArgumentException("target == null");
+      } else if (targetDir == null) {
+         throw new IllegalArgumentException("targetDir == null");
+      }
+
+      // Perform transformations
+      transformToJava(def, target + '/', targetDir, "Log");
+      transformToJava(def, "",           targetDir, "TranslationBundle");
+      for (Map.Entry<String,Document> entry : def.getTranslations().entrySet()) {
+         String           locale = entry.getKey();
+         Document translationXML = entry.getValue();
+         transformToJavaForLocale(def, targetDir, locale, translationXML);
+      }
+   }
+
+   private void transformToJava(LogDef def, String xsltSubDir, File targetDir, String className)
+   throws IOException {
+
+      Source      source = new DOMSource(def.getXML());
+      String    xsltPath = xsltSubDir + "log_to_" + className + "_java" + ".xslt";
+      String  domainPath = def.getDomainName().replace(".", "/");
+      File        outDir = new File(targetDir, domainPath);
+      String outFileName = className + ".java";
+
+      Map<String,String> xsltParams = new HashMap<String,String>();
+      xsltParams.put("package_name", def.getDomainName());
+      xsltParams.put("accesslevel",  def.isPublic() ? "public" : "protected");
+
+      new Xformer(_resolver).transformAndHandleExceptions(source, xsltPath, xsltParams, outDir, outFileName);
+   }
+
+   private void transformToJavaForLocale(LogDef def, File targetDir, String locale, Document translationXML)
+   throws IOException {
+
+      Source      source = new DOMSource(translationXML);
+      String    xsltPath = "translation-bundle_to_java.xslt";
+      String outFileName = "TranslationBundle_" + locale + ".java";
+
+      Map<String,String> xsltParams = new HashMap<String,String>();
+      xsltParams.put("package_name", def.getDomainName());
+      xsltParams.put("accesslevel",  def.isPublic() ? "public" : "protected");
+      xsltParams.put("locale",       locale);
+
+      new Xformer(_resolver).transformAndHandleExceptions(source, xsltPath, xsltParams, targetDir, outFileName);
+   }
+}
---
+++ base/src/main/java/org/znerd/logdoc/gen/Xformer.java
@@ -0,0 +1,82 @@
+// See the COPYRIGHT file for copyright and license information
+package org.znerd.logdoc.gen;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.znerd.logdoc.Library;
+import org.znerd.logdoc.LogLevel;
+import org.znerd.logdoc.Resolver;
+
+import static org.znerd.logdoc.internal.ExceptionUtils.newIOException;
+import static org.znerd.logdoc.internal.InternalLogging.log;
+
+class Xformer {
+
+   Xformer(Resolver resolver) {
+      _resolver = resolver;
+   }
+
+   final Resolver _resolver;
+
+   final void transformAndHandleExceptions(Source source, String xsltPath, Map<String,String> xsltParams, File outDir, String outFileName) throws IOException {
+      try {
+         transform(source, xsltPath, xsltParams, outDir, outFileName);
+      } catch (TransformerConfigurationException cause) {
+         throw newIOException("Unable to perform XSLT transformation due to configuration problem.", cause);
+      } catch (TransformerException cause) {
+         throw newIOException("Failed to perform XSLT transformation.", cause);
+      }
+   }
+
+   private final void transform(Source source, String xsltPath, Map<String,String> xsltParams, File outDir, String outFileName) throws TransformerConfigurationException, TransformerException, IOException {
+
+      Transformer xformer = createTransformer(xsltPath);
+      setTransformerParameters(xformer, xsltParams);
+      assertOutputDirectory(outDir);
+
+      File        outFile = new File(outDir, outFileName);
+      StreamResult result = new StreamResult(outFile);
+
+      log(LogLevel.INFO, "Generating file \"" + outFile.getPath() + "\" using stylesheet \"" + xsltPath + "\".");
+      xformer.transform(source, result);
+      log(LogLevel.INFO, "Generated file \"" + outFile.getPath() + "\" using stylesheet \"" + xsltPath + "\".");
+   }
+
+   private final Transformer createTransformer(String xsltPath) throws TransformerConfigurationException, IOException {
+      TransformerFactory xformerFactory = TransformerFactory.newInstance();
+      xformerFactory.setURIResolver(_resolver);
+
+      InputStream        xsltStream = Library.getMetaResourceAsStream("xslt/" + xsltPath);
+      StreamSource xsltStreamSource = new StreamSource(xsltStream);
+
+      return xformerFactory.newTransformer(xsltStreamSource);
+   }
+
+   private final void setTransformerParameters(Transformer xformer, Map<String,String> params) {
+      for (String key : params.keySet()) {
+         xformer.setParameter(key, params.get(key));
+      }
+   }
+
+   private final void assertOutputDirectory(File outDir) throws IOException {
+      if (! outDir.exists()) {
+         boolean outDirCreated = outDir.mkdirs();
+         if (! outDirCreated) {
+            throw new IOException("Failed to create output directory \"" + outDir.getPath() + "\".");
+         }
+      } else if (! outDir.isDirectory()) {
+         throw new IOException("Path \"" + outDir.getPath() + "\" exists, but it is not a directory.");
+      }
+   }
+}
