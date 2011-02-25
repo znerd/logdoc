e8627ff5241191c17f8df264fc64737673ae80c0
Ernst de Haan <ernst@ernstdehaan.com>
2011-02-25 13:53:13 +0100

    Made the DocGenerator stateful.

--- base/src/main/java/org/znerd/logdoc/gen/DocGenerator.java
+++ base/src/main/java/org/znerd/logdoc/gen/DocGenerator.java
@@ -35,60 +35,78 @@ import org.znerd.logdoc.internal.Resolver;
 public final class DocGenerator {
 
    /**
-    * Generates the HTML documentation for the specified log definition.
+    * Creates a new <code>DocGenerator</code> for the specified log 
+    * definition.
     *
     * @param def
     *    the {@link LogDef} object, cannot be <code>null</code>.
     *
     * @param targetDir
     *    the target directory to create the HTML documentation files in,
-    *    cannot be <code>null</code>, and must be an existent writable
-    *    directory.
+    *    cannot be <code>null</code>.
     *
     * @throws IllegalArgumentException
-    *    if <code>targetDir == null</code>.
-    *
-    * @throws IOException
-    *    if the HTML documentation files could not be generated.
+    *    if <code>def == null || targetDir == null</code>.
     */
-   public void generateHtml(LogDef def, File targetDir)
-   throws IllegalArgumentException, IOException {
-
-      // Check preconditions
-      if (targetDir == null) {
-         throw new IllegalArgumentException("targetDir == null");
+   public DocGenerator(LogDef def, File targetDir) throws IllegalArgumentException {
+      if (def == null) {
+         throw new IllegalArgumentException("def == null");
       } else if (targetDir == null) {
          throw new IllegalArgumentException("targetDir == null");
       }
 
-      transformToHtml(def, targetDir, "",      "index"     , new HashMap<String,String>());
-      transformToHtml(def, targetDir, "_list", "entry-list", new HashMap<String,String>());
+      _def       = def;
+      _targetDir = targetDir;
+   }
 
-      for (LogDef.Group group : def.getGroups()) {
+   private final LogDef _def;
+   private final File _targetDir;
+
+   /**
+    * Generates the HTML documentation for the specified log definition.
+    *
+    * @throws IOException
+    *    if the HTML documentation files could not be generated.
+    */
+   public void generateDoc()
+   throws IllegalArgumentException, IOException {
+
+      transformToDoc("",      "index"     , new HashMap<String,String>());
+      transformToDoc("_list", "entry-list", new HashMap<String,String>());
+
+      transformGroupsAndEntries();
+   }
+
+   private final void transformGroupsAndEntries() throws IOException {
+      for (LogDef.Group group : _def.getGroups()) {
          String groupID = group.getID();
          Map<String,String> xsltParams = new HashMap<String,String>();
          xsltParams.put("group", groupID);
          String stylesheetName = "_group";
-         String outName = "group-" + groupID;
-         transformToHtml(def, targetDir, stylesheetName, outName, xsltParams);
+         String        outName = "group-" + groupID;
+         transformToDoc(stylesheetName, outName, xsltParams);
 
-         for (LogDef.Entry entry : group.getEntries()) {
-            String entryID = entry.getID();
-            xsltParams = new HashMap<String,String>();
-            xsltParams.put("entry", entryID);
-            stylesheetName = "_entry";
-            outName = "entry-" + entryID;
-            transformToHtml(def, targetDir, stylesheetName, outName, xsltParams);
-         }
+         transformEntries(group);
       }
    }
 
-   private final void transformToHtml(LogDef def, File targetDir, String stylesheetName, String outName, Map<String,String> xsltParams)
+   private final void transformEntries(LogDef.Group group) throws IOException {
+      for (LogDef.Entry entry : group.getEntries()) {
+         String entryID = entry.getID();
+         Map<String,String> xsltParams = new HashMap<String,String>();
+         xsltParams.put("entry", entryID);
+         String stylesheetName = "_entry";
+         String outName = "entry-" + entryID;
+         transformToDoc(stylesheetName, outName, xsltParams);
+      }
+   }
+
+   private final void transformToDoc(String stylesheetName, String outName, Map<String,String> xsltParams)
    throws IOException {
-      Source      source = new DOMSource(def.getXML());
+      Source      source = new DOMSource(_def.getXML());
       String    xsltPath = "log_to" + stylesheetName + "_html.xslt";
       String outFileName = outName + ".html";
 
-      new Xformer(def).transformAndHandleExceptions(source, xsltPath, xsltParams, targetDir, outFileName);
+      new Xformer(_def).transformAndHandleExceptions(source, xsltPath, xsltParams, _targetDir, outFileName);
    }
 }
