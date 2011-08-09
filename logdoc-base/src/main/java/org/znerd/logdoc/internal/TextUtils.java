cb8efe3975b74dea5eb8624e7abb07b3bb7be660
Ernst de Haan <ernst@ernstdehaan.com>
2011-08-09 22:55:32 +0200

    Removed superfluous Javadoc comments.

--- logdoc-base/src/main/java/org/znerd/logdoc/internal/TextUtils.java
+++ logdoc-base/src/main/java/org/znerd/logdoc/internal/TextUtils.java
@@ -2,23 +2,11 @@
 package org.znerd.logdoc.internal;
 
 public class TextUtils {
-    
-    /**
-     * Returns a quoted version of the specified string, or <code>"(null)"</code> if the argument is <code>null</code>.
-     * 
-     * @param s the character string, can be <code>null</code>, e.g. <code>"foo bar"</code>.
-     * @return the quoted string, e.g. <code>"\"foo bar\""</code>, or <code>"(null)"</code> if the argument is <code>null</code>.
-     */
+
     public static final String quote(String s) {
         return s == null ? "(null)" : "\"" + s + '"';
     }
 
-    /**
-     * Returns a quoted version string representation, or <code>"(null)"</code> if the argument is <code>null</code>.
-     * 
-     * @param o the object, can be <code>null</code>.
-     * @return the quoted string representation of the specified object, e.g. <code>"\"foo bar\""</code>, or <code>"(null)"</code> if the argument is <code>null</code>.
-     */
     public static final String quote(Object o) {
         return o == null ? "(null)" : quote(o.toString());
     }
