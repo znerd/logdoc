8a93d6763cabeec0e80b46c9aefc5836d00f88ed
Ernst de Haan <ernst@ernstdehaan.com>
2011-08-09 23:02:36 +0200

    Style improvements, in accordance with Clean Code.

--- logdoc-base/src/main/java/org/znerd/logdoc/internal/InternalLogging.java
+++ logdoc-base/src/main/java/org/znerd/logdoc/internal/InternalLogging.java
@@ -5,94 +5,48 @@ import org.znerd.logdoc.LogLevel;
 
 /**
  * Logdoc-internal logging.
+ * <p>
+ * When <code>null</code> is passed as a {@link LogLevel}, then {@link LogLevel#DEBUG} is assumed.
+ * When <code>null</code> is passed as a message, then an empty string is assumed.
  */
 public class InternalLogging {
 
-   private static InternalLogging INSTANCE = new InternalLogging();
+    public static synchronized void log(LogLevel level, String message) {
+        log(level, message, (Throwable) null);
+    }
 
-   /**
-    * Constructs a new <code>InternalLogging</code> object.
-    */
-   protected InternalLogging() {
-      // empty
-   }
+    public static synchronized void log(LogLevel level, String message, Throwable exception) {
+        if (level == null) {
+            level = LogLevel.DEBUG;
+        }
+        if (message == null) {
+            message = "";
+        }
 
-   /**
-    * Logs a message at a specified level.
-    *
-    * @param level
-    *    the {@link LogLevel}, <code>null</code> is equivalent to passing
-    *    {@link LogLevel#DEBUG}.
-    *
-    * @param message
-    *    the message, or <code>null</code>.
-    */
-   public static void log(LogLevel level, String message) {
-      log(level, message, (Throwable) null);
-   }
+        INSTANCE.logImpl(level, message, exception);
+    }
 
-   /**
-    * Logs a message at a specified level, with an optional exception.
-    *
-    * @param level
-    *    the {@link LogLevel}, <code>null</code> is equivalent to passing
-    *    {@link LogLevel#DEBUG}.
-    *
-    * @param message
-    *    the message, or <code>null</code> if an empty message should be
-    *    logged.
-    *
-    * @param exception
-    *    the exception to log, or <code>null</code> if there is no exception
-    *    to log.
-    */
-   public static synchronized void log(LogLevel level, String message, Throwable exception) {
-      if (level == null) {
-         level = LogLevel.DEBUG;
-      }
-      if (message == null) {
-         message = "";
-      }
+    private static InternalLogging INSTANCE = new InternalLogging();
 
-      INSTANCE.logImpl(level, message, exception);
-   }
+    public static synchronized void setLogger(InternalLogging logger) throws IllegalArgumentException {
+        if (logger == null) {
+            throw new IllegalArgumentException("logger == null");
+        }
+        INSTANCE = logger;
+    }
 
-   /**
-    * Logs a message at a specified level, with an optional exception.
-    *
-    * @param level
-    *    the {@link LogLevel}, <code>null</code> is equivalent to passing
-    *    {@link LogLevel#DEBUG}.
-    *
-    * @param message
-    *    the message, or <code>null</code> if an empty message should be
-    *    logged.
-    *
-    * @param exception
-    *    the exception to log, or <code>null</code> if there is no exception
-    *    to log.
-    */
-   public void logImpl(LogLevel level, String message, Throwable exception) {
-      System.err.println(level.name() + ' ' + message);
-      if (exception != null) {
-         exception.printStackTrace();
-      } 
-   }
+    public void logImpl(LogLevel level, String message, Throwable exception) {
+        fallbackLogImpl(level, message, exception);
+    }
 
-   /**
-    * Sets the <code>InternalLogging</code> instance to use.
-    *
-    * @param logger
-    *    the instance to use, cannot be <code>null</code>.
-    *
-    * @throws IllegalArgumentException
-    *    if <code>logger == null</code>.
-    */
-   public static synchronized void setLogger(InternalLogging logger)
-   throws IllegalArgumentException {
-      if (logger == null) {
-         throw new IllegalArgumentException("logger == null");
-      }
-      INSTANCE = logger;
-   }
+    private void fallbackLogImpl(LogLevel level, String message, Throwable exception) {
+        System.err.println(level.name() + ' ' + message);
+        if (exception != null) {
+            exception.printStackTrace();
+        }
+    }
+
+    protected InternalLogging() {
+        // empty
+    }
 }
