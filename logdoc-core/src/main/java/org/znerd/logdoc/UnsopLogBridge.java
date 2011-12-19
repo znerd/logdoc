7e270a104e610349268257d378ce3696f90e57b7
Ernst de Haan <ernst@ernstdehaan.com>
2011-12-19 21:39:56 +0100

    New UnsopLogBridge class.

---
+++ logdoc-core/src/main/java/org/znerd/logdoc/UnsopLogBridge.java
@@ -0,0 +1,34 @@
+// See the COPYRIGHT file for copyright and license information
+package org.znerd.logdoc;
+
+import org.znerd.util.log.LogLevel;
+
+/**
+ * Implementation of a <code>LogBridge</code> that throws an <code>UnsupportedOperationException</code> from each method.
+ */
+public class UnsopLogBridge extends LogBridge {
+    @Override
+    public void putContextId(String newContextId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void unputContextId() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getContextId() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean shouldLog(String domain, String groupId, String entryId, LogLevel level) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void logOneMessage(String fqcn, String domain, String groupId, String entryId, LogLevel level, String message, Throwable exception) {
+        throw new UnsupportedOperationException();
+    }    
+}
